f5f93b6069ef5cee3b5b7a38ccb0db575c7f1faf
alexey-super-dev <alexey.kurylev@gmail.com>
2025-05-21 18:44:51 -0300

    Update views.py

--- project_run/views.py
+++ project_run/views.py
@@ -2,14 +2,15 @@ from django.http import JsonResponse
 from rest_framework import viewsets
 from project_run.models import Run
 from project_run.serializers import RunSerializer
+from django.conf import settings
 
 def company_details(request):
     """
     API endpoint that returns company details in JSON format.
     """
     data = {
-        'company_name': 'Awesome Tech Solutions',
-        'slogan': 'Innovating for a better tomorrow',
+        'company_name': settings.COMPANY_NAME,
+        'slogan': settings.SLOGAN,
         'contacts': 'info@awesometech.com, +1-123-456-7890, 123 Tech Street, Silicon Valley'
     }
     return JsonResponse(data)
