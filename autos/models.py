fd67fe9ee2c399341dcef99d67d234685c44dcb9
Alexey Kurylev <chepe4pi@gmail.com>
2025-05-31 22:36:09 -0300

    add error

--- autos/models.py
+++ autos/models.py
@@ -8,7 +8,7 @@ class Autos(models.Model):
 
 
 class Run(models.Model):
-    comment = models.CharField(max_length=250, null=True)
+    comment = models.CharField(max_length=250)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=50, default='init')
