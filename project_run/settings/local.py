f624d9e07a8446ef285ceddfba5c278988b82912
Alexey Kurylev <chepe4pi@gmail.com>
2025-06-11 16:51:55 -0300

    add error

--- project_run/settings/local.py
+++ project_run/settings/local.py
@@ -16,3 +16,8 @@ INSTALLED_APPS += [
 MIDDLEWARE += [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+# IP addresses that are allowed to see the debug toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- project_run/urls.py
+++ project_run/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -42,3 +43,10 @@ urlpatterns = [
 
     path('api/', include(router.urls))
 ]
+
+# Add Django Debug Toolbar URLs when in debug mode
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
