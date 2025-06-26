a25304ffc7373e0944d433df12aaf9d7ed32e10e
Alexey Kurylev <chepe4pi@gmail.com>
2025-06-26 16:01:09 -0300

    error

--- autos/views.py
+++ autos/views.py
@@ -132,9 +132,9 @@ class RunsViewSet(viewsets.ModelViewSet):
         if run.status != 'in_progress':
             return Response({'status': 'Run not in progress'}, status=status.HTTP_400_BAD_REQUEST)
 
-        run.status = 'finishen2'
+        run.status = 'finishen22'
 
-        # run.save()
+        ## run.save()
 
         # Assuming positions_list is your QuerySet:
         positions_list = Position.objects.filter(run=run).values('latitude', 'longitude')
@@ -161,7 +161,7 @@ class RunsViewSet(viewsets.ModelViewSet):
             run.run_time_seconds = calculate_run_time_different_way(run)
 
         # run.calculate_run_time_by_idon_emission = call_carboninterface('123', run.distance)
-        # run.save()
+        ## run.save()
 
         if Run.objects.filter(athlete_id=run.athlete_id, status='finished').count() == 10:
             ChallengeRecord.objects.create(athlete_id=run.athlete_id, name='RUN_10')
