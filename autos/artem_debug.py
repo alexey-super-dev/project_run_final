897cd4baabbd53ff8d87e846c114fa06a6fb6f3e
Alexey Kurylev <chepe4pi@gmail.com>
2025-07-03 11:44:38 -0300

    check code

---
+++ autos/artem_debug.py
@@ -0,0 +1,68 @@
+from openpyxl.reader.excel import load_workbook
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from autos.models import CollectableItem
+
+
+def latitude_validator(value):
+    if value < -90 or value > 90:
+        raise serializers.ValidationError("Широта должна находиться в диапазоне от -90.0 до +90.0 градусов")
+    return value
+
+
+def longitude_validator(value):
+    if value < -180 or value > 180:
+        raise serializers.ValidationError("Долгота должна находиться в диапазоне от -180.0 до +180.0 градусов")
+    return value
+
+
+class CollectibleItemSerializer(serializers.ModelSerializer):
+    url = serializers.URLField(source='picture')
+
+    class Meta:
+        model = CollectableItem
+        exclude = ["picture"]
+        extra_kwargs = {
+            "latitude": {
+                "validators": [latitude_validator],
+            },
+            "longitude": {
+                "validators": [longitude_validator],
+            },
+        }
+
+
+class FileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField(write_only=True, required=True)
+
+
+class UploadFileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        input_serializer = FileUploadSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        file_object = input_serializer.validated_data["file"]
+        workbook = load_workbook(file_object)
+        worksheet = workbook.active
+
+        header_row = next(worksheet.iter_rows(min_row=1, max_row=1, values_only=True))
+        headers = list(h.lower() for h in header_row)
+
+        wrong_rows = list()
+
+        for row in worksheet.iter_rows(min_row=2, values_only=True):
+            row_serializer = CollectibleItemSerializer(data=dict(zip(headers, row)))
+            if row_serializer.is_valid(raise_exception=False):
+                row_serializer.save()
+            else:
+                wrong_rows.append(list(row))
+
+        return Response(wrong_rows, status=status.HTTP_200_OK)
--- project_run/urls.py
+++ project_run/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from autos.artem_debug import UploadFileView
 from autos.views import get_autos, RunsViewSet, get_company_details, PositionViewSet, UsersViewSet, \
     subscribe_to_coach_api_url, ChallengeViewSet, get_challenges_summary, rate_coach, analytics_for_coach, \
     AthleteInfoViewSet, UploadXLSX, CollectableItemViewSet, get_challenges
@@ -40,7 +41,8 @@ urlpatterns = [
     # path('api/challenges/', get_challenges, name='challenges_summary'),
     path('api/rate_coach/<int:coach_id>/', rate_coach, name='rate_coach'),
     path('api/analytics_for_coach/<int:coach_id>/', analytics_for_coach, name='analytics_for_coach'),
-    path('api/upload_file/', UploadXLSX.as_view(), name='unit_location_upload'),
+    # path('api/upload_file/', UploadXLSX.as_view(), name='unit_location_upload'),
+    path('api/upload_file/', UploadFileView.as_view(), name='unit_location_upload'),
 
     path('api/', include(router.urls))
 ]
